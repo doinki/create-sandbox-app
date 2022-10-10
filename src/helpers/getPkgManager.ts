144c2d660cb49740ba32f278d59403c2821900f4
doinki <doinki94@gmail.com>
2022-10-11 00:50:21 +0900

    🚀

--- src/helpers/getPkgManager.ts
+++ src/helpers/getPkgManager.ts
@@ -5,10 +5,12 @@ export type PackageManager = 'npm' | 'pnpm' | 'yarn';
 const getPkgManager = (): PackageManager => {
   try {
     const userAgent = process.env.npm_config_user_agent;
+
     if (userAgent) {
       if (userAgent.startsWith('yarn')) {
         return 'yarn';
-      } else if (userAgent.startsWith('pnpm')) {
+      }
+      if (userAgent.startsWith('pnpm')) {
         return 'pnpm';
       }
     }
