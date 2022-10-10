a3666ba5f2ef4cf3053ad88448c4209431b0b84c
doinki <doinki94@gmail.com>
2022-10-11 01:50:44 +0900

    🚀

--- src/createApp.ts
+++ src/createApp.ts
@@ -1,15 +1,15 @@
 import chalk from 'chalk';
 import cpy from 'cpy';
-import { writeFileSync } from 'node:fs';
+import { readFileSync, writeFileSync } from 'node:fs';
 import { EOL } from 'node:os';
 import { basename, dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import { PackageManager } from './helpers/getPkgManager';
+import type { PackageManager } from './helpers/getPkgManager';
 import tryGitInit from './helpers/git';
+import install from './helpers/install';
 import isFolderEmpty from './helpers/isFolderEmpty';
 import getOnline from './helpers/isOnline';
-import install from './helpers/install';
 import isWriteable from './helpers/isWriteable';
 import makeDir from './helpers/makeDir';
 
@@ -18,18 +18,19 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const createApp = async ({
   appPath,
   packageManager,
+  template = 'default',
 }: {
   appPath: string;
   packageManager: PackageManager;
+  template: string;
 }): Promise<void> => {
   const root = resolve(appPath);
 
   if (!isWriteable(dirname(root))) {
     console.error(
       `The application path is not writable, please check folder permissions and try again.
-      It is likely you do not have write permissions for this folder.`
+It is likely you do not have write permissions for this folder.`
     );
-
     process.exit(1);
   }
 
@@ -50,25 +51,14 @@ const createApp = async ({
 
   console.log(chalk.bold(`Using ${packageManager}.`));
 
+  const templatePackageJson = JSON.parse(
+    readFileSync(join(__dirname, 'templates', template, 'package.json'), 'utf8')
+  );
   const packageJson = {
     name: appName,
     private: true,
-    scripts: {
-      analyze: 'ANALYZE=true yarn build',
-      build: 'next build',
-      dev: 'next dev',
-      lint: 'next lint',
-      start: 'next start',
-      test: 'jest --coverage',
-      'test:watch': 'jest --watch',
-    },
     sideEffects: false,
-    resolutions: {
-      '@types/node': '^16.0.0',
-      postcss: '^8.0.0',
-    },
-    dependencies: {},
-    devDependencies: {},
+    ...templatePackageJson,
   };
 
   writeFileSync(
@@ -76,88 +66,49 @@ const createApp = async ({
     JSON.stringify(packageJson, null, 2) + EOL
   );
 
-  const installFlags = { packageManager, isOnline };
-  const dependencies = [
-    'react',
-    'react-dom',
-    'next',
-    '@next/bundle-analyzer',
-    'next-seo',
-    'clsx',
-  ];
-  const devDependencies = [
-    '@jest/types',
-    '@next/eslint-plugin-next',
-    '@testing-library/dom',
-    '@testing-library/jest-dom',
-    '@testing-library/react',
-    '@testing-library/user-event',
-    '@types/jest',
-    '@types/node@^16.0.0',
-    '@types/react',
-    '@types/react-dom',
-    '@types/testing-library__jest-dom',
-    '@typescript-eslint/eslint-plugin',
-    '@typescript-eslint/parser',
-    'eslint',
-    'eslint-config-airbnb',
-    'eslint-config-airbnb-typescript',
-    'eslint-config-prettier',
-    'eslint-import-resolver-typescript',
-    'eslint-plugin-import',
-    'eslint-plugin-jest',
-    'eslint-plugin-jest-dom',
-    'eslint-plugin-jsx-a11y',
-    'eslint-plugin-prettier',
-    'eslint-plugin-react',
-    'eslint-plugin-react-hooks',
-    'eslint-plugin-simple-import-sort',
-    'eslint-plugin-sort-destructure-keys',
-    'eslint-plugin-sort-keys-fix',
-    'eslint-plugin-tailwindcss',
-    'eslint-plugin-testing-library',
-    'eslint-plugin-trim',
-    'eslint-plugin-typescript-sort-keys',
-    'jest',
-    'jest-environment-jsdom',
-    'postcss',
-    'postcss-flexbugs-fixes',
-    'postcss-preset-env',
-    'prettier',
-    'tailwindcss',
-    'typescript',
-  ];
+  const installFlags = { isOnline, packageManager };
+  const { dependencies = {}, devDependencies = {} } = templatePackageJson as {
+    dependencies?: object;
+    devDependencies?: object;
+  };
 
   console.log(
-    dependencies.reduce(
-      (prev, curr) => prev + `- ${chalk.cyan(curr)}\n`,
+    Object.keys(dependencies).reduce(
+      (prev, curr) => `${prev}- ${chalk.cyan(curr)}\n`,
       `\nInstalling dependencies:\n`
     )
   );
 
-  await install(root, dependencies, installFlags);
+  await install(root, Object.keys(dependencies), installFlags);
 
   console.log(
-    devDependencies.reduce(
-      (prev, curr) => prev + `- ${chalk.cyan(curr)}\n`,
+    Object.keys(devDependencies).reduce(
+      (prev, curr) => `${prev}- ${chalk.cyan(curr)}\n`,
       `\nInstalling devDependencies:\n`
     )
   );
 
-  await install(root, devDependencies, {
+  await install(root, Object.keys(devDependencies), {
     devDependencies: true,
     ...installFlags,
   });
 
-  await cpy(join(__dirname, 'templates', 'default', '**'), root, {
+  await cpy(join(__dirname, 'templates', template, '**'), root, {
     dot: true,
+    filter: (file) => {
+      return file.name !== 'package.json';
+    },
+    rename: (name) => {
+      return name === 'gitignore' ? `.${name}` : name;
+    },
   });
 
   if (tryGitInit()) {
-    console.log('\nInitialized a git repository.\n');
+    console.log('\nInitialized a git repository.');
   }
 
-  let cdpath = join(originalDirectory, appName) === appPath ? appName : appPath;
+  const cdpath =
+    join(originalDirectory, appName) === appPath ? appName : appPath;
 
   console.log(`\n${chalk.green('Success!')} Created ${appName} at ${appPath}
 Inside that directory, you can run several commands:
@@ -174,8 +125,7 @@ Inside that directory, you can run several commands:
 We suggest that you begin by typing:
 
   ${chalk.cyan('cd')} ${cdpath}
-  ${chalk.cyan(`${packageManager} ${useYarn ? '' : 'run '}dev`)}
-`);
+  ${chalk.cyan(`${packageManager} ${useYarn ? '' : 'run '}dev`)}\n`);
 };
 
 export default createApp;
